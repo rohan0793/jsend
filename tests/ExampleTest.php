06f9c240ddffbf9fb9555f742872988fd27f5347
Rohan Chhabra <rohan0793@gmail.com>
2020-07-16 20:28:24 +0530

    git remove class from test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,16 +3,9 @@
 namespace RC\Jsend\Tests;
 
 use Orchestra\Testbench\TestCase;
-use RC\Jsend\ResponseMacroServiceProvider;
 
 class ExampleTest extends TestCase
 {
-
-    protected function getPackageProviders($app)
-    {
-        return [ResponseMacroServiceProvider::class];
-    }
-    
     /** @test */
     public function true_is_true()
     {
